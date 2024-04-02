4dcb2468a512123fcfa087c678de814e433e2539
skoczylass <125471975+skoczylass@users.noreply.github.com>
2024-04-02 13:42:26 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,12 +1,10 @@
 import pandas as pd
 import os
 
-# Funkcja do scalania ramek danych
 def merge_dataframes(df1, df2):
     merged_df = pd.merge(df1, df2, on="Pos_ref_alt", how="inner")
     return merged_df
 
-# Funkcja do znalezienia unikalnych i powtarzających się wierszy
 def find_unique_duplicates(df1, df2):
     non_matching_df_unique = df1[~df1["Pos_ref_alt"].isin(df2["Pos_ref_alt"])].copy()
     non_matching_df_unique.drop_duplicates(subset="Pos_ref_alt", keep=False, inplace=True)
@@ -16,21 +14,18 @@ def find_unique_duplicates(df1, df2):
 
     return non_matching_df_unique, non_matching_df_multi
 
-# Funkcja do zapisu danych do pliku tekstowego
 def write_to_text(dataframe, text_path, comment=None):
     if comment:
         with open(text_path, 'w') as file:
             file.write(f"# {comment}\n")
     dataframe.to_csv(text_path, sep='\t', index=False, mode='a', header=not os.path.exists(text_path))
 
-# Funkcja do mapowania pozycji na locus
 def map_position_to_locus(position, loci_df):
     for index, row in loci_df.iterrows():
         if row['Starting'] <= position <= row['Ending']:
             return f"{row['Map Locus']}\t{row['Description']}"
     return "Unknown\tUnknown"
 
-# Funkcja do dodawania zmapowanych danych
 def add_mapped_data(file_path, loci_df):
     with open(file_path, 'r') as file:
         lines = file.readlines()
@@ -46,27 +41,22 @@ def add_mapped_data(file_path, loci_df):
 
     with open(file_path, 'w') as output_file:
         output_file.write('\n'.join(mapped_lines))
-
-# Pozostałe funkcje i definicje stałych
-
-# Definitions of constants
+        
 BASE_DIR = r"path\mtDNA_analysis_mtDNAServer"
 INPUT_DIR = os.path.join(BASE_DIR, "Input")
 RESULTS_DIR = os.path.join(BASE_DIR, "Results")
 DATABASES_DIR = os.path.join(BASE_DIR, "Database")
 
-# Make sure the Results directory exists
 if not os.path.exists(RESULTS_DIR):
     os.makedirs(RESULTS_DIR)
-
-# Additional Step: Remove existing files in the Results directory
+    
 existing_files = os.listdir(RESULTS_DIR)
 for file in existing_files:
     file_path = os.path.join(RESULTS_DIR, file)
     if os.path.isfile(file_path):
         os.remove(file_path)
 
-# Step 0: Sort variants.txt by "Pos" in descending order
+# Step 0
 input_txt_path = os.path.join(INPUT_DIR, "variants.txt")
 df_variants = pd.read_csv(input_txt_path, sep='\t')
 df_variants = df_variants.sort_values(by='Pos', ascending=True)  # Change to ascending order
@@ -109,7 +99,7 @@ write_to_text(non_matching_df_multi_step_3, output_path_step_3)
 df_helix = pd.read_csv(os.path.join(DATABASES_DIR, "HelixMTdb.txt"), sep='\t')
 merged_df_step_3_5 = merge_dataframes(df_step_1, df_helix)
 
-# Create a new file containing only matched variants where df_step_1 is common for both dataframes
+# Create a new file containing only matched variants
 output_path_step_3_5 = os.path.join(RESULTS_DIR, "Matched Variants in HelixMTdb.txt")
 
 if not merged_df_step_3_5.empty:
@@ -127,21 +117,19 @@ matched_df_step_4 = merged_df_step_4[merged_df_step_4['Gene_symbol'].notnull()]
 
 write_to_text(matched_df_step_4, output_text_path_step_4, comment="No prediction found")
 
-# Step 5.1: Compare Step_1.txt with gnomad.txt
+# Step 5.1
 df_gnomad = pd.read_csv(os.path.join(DATABASES_DIR, "gnomad.txt"), sep='\t')
 
 # Merge Step_1.txt with gnomad.txt based on Pos_ref_alt
 merged_df_step_5_1 = merge_dataframes(df_step_1, df_gnomad)
 
-# Identify non-matching rows (Step_1.txt entries not present in gnomad.txt)
 non_matching_df_step_5_1 = df_step_1[~df_step_1["Pos_ref_alt"].isin(df_gnomad["Pos_ref_alt"])].copy()
 
-# Save non-matching entries to "Zero Frequency in gnomad_Unique.txt"
 output_path_step_5_1_zero_frequency_unique = os.path.join(RESULTS_DIR, "Zero Frequency in gnomAD_Unique.txt")
 non_matching_df_step_5_1_unique = non_matching_df_step_5_1[~non_matching_df_step_5_1["Pos_ref_alt"].duplicated(keep=False)].copy()
 write_to_text(non_matching_df_step_5_1_unique, output_path_step_5_1_zero_frequency_unique)
 
-# Save repeated non-matching entries to "Zero Frequency in gnomad_Repeats.txt"
+
 output_path_step_5_1_zero_frequency_repeats = os.path.join(RESULTS_DIR, "Zero Frequency in gnomAD_Repeats.txt")
 non_matching_df_step_5_1_repeats = non_matching_df_step_5_1[non_matching_df_step_5_1["Pos_ref_alt"].duplicated(keep=False)].copy()
 write_to_text(non_matching_df_step_5_1_repeats, output_path_step_5_1_zero_frequency_repeats)
@@ -154,7 +142,7 @@ else:
     with open(output_path_step_5_1_matched_variants, 'w') as file:
         file.write("No matched variants found in gnomAD.")
 
-# Step 5.2: Create a combined file with information from Step_1.txt and gnomad.txt
+# Step 5.2
 combined_df_step_5_2 = pd.concat([merged_df_step_5_1, non_matching_df_step_5_1])
 
 # Load data from "Loci.txt" file
